7c5505aef0bcfe50a14a6cd2f7f7a7389d67e401
Mohammadreza Rashidpour <rashidpour1995@gmail.com>
2024-12-21 16:38:24 +0330

    first commit test

---
+++ mrtestplugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * mycheckupdate
+ *
+ * Plugin Name: mrtestplugin
+ * Plugin URI:  https://mrrashidpour.com
+ * Description: This system will be converted into data analysis in the future, now it only intends to check production plugins.
+ * Version:     1.1.0
+ * Author:      Mohammadreza Rashidpour Aghamahali
+ * Author URI:  https://mrrashidpour.com
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: mrrashidpour
+ * Domain Path: /languages
+ * Requires at least: 6.2
+ * Requires PHP: 7.4
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
